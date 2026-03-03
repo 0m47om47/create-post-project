9122b5f0baaa860d40330be6d2355b61cb380b4e
0m47om47 <choubeyom873@gmail.com>
2026-03-03 10:52:04 +0530

    Revert "initialize server, app, and database connection"
    This reverts commit 56f004c4071c29a9c44e284d5fbbec752c07a2a1.

--- backend/src/db/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose");
-async function connectDB(){
-    await mongoose.connect("mongodb+srv://choubeyom873_db_user:Post47post47@cluster0.np6a2po.mongodb.net/project-1")
-    console.log("DB connected");
-}
-module.exports=connectDB;
