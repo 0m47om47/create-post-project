0277c588627653e91cfbc69693d3d7689d671cdb
0m47om47 <choubeyom873@gmail.com>
2026-04-05 23:19:21 +0530

    added

---
+++ backend/services/storage.service.js
@@ -0,0 +1,15 @@
+const ImageKit=require('@imagekit/nodejs');
+
+const client = new ImageKit({
+  privateKey: process.env['IMAGEKIT_PRIVATE_KEY'],
+});
+
+
+async function uploadFile(buffer) {
+const response = await client.files.upload({
+  file:buffer,
+  fileName: 'image.jpg',
+});
+return response;
+}
+module.exports=uploadFile;
